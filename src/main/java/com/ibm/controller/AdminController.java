1114868dbc3deb95fe2dfcdccd462c32dfcda1da
Hyejin Lee <hyejlee@kr.ibm.com>
2018-03-04 15:55:18 +0900

    commit

--- src/main/java/com/ibm/controller/AdminController.java
+++ src/main/java/com/ibm/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.ibm.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.ibm.domain.BoardDomain;
 import com.ibm.service.AdminService;
 
 @Controller
@@ -13,4 +19,12 @@ public class AdminController {
 	@Autowired
 	AdminService adminService;
 	
+	@GetMapping("/index")
+	public ModelAndView admin() throws Exception{
+		ModelAndView mv = new ModelAndView("/admin/index");
+		List<BoardDomain> resultList = new ArrayList<>();
+		//resultList = adminService.selectBoardList();
+		//mv.addObject("boardList", resultList);
+		return mv;
+	}
 }
