421f6d5b996e78f03ed66418cece579620885dbc
YulimKang <YulimKang@kr055298>
2018-04-01 20:58:45 +0900

    add alert

---
+++ src/main/java/com/ibm/domain/AlertDomain.java
@@ -0,0 +1,35 @@
+package com.ibm.domain;
+
+public class AlertDomain {
+	
+	String alertid;
+	String memberid;
+	String projectid;
+	String confirmyn;
+	
+	public String getAlertid() {
+		return alertid;
+	}
+	public void setAlertid(String alertid) {
+		this.alertid = alertid;
+	}
+	public String getMemberid() {
+		return memberid;
+	}
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+	public String getProjectid() {
+		return projectid;
+	}
+	public void setProjectid(String projectid) {
+		this.projectid = projectid;
+	}
+	public String getConfirmyn() {
+		return confirmyn;
+	}
+	public void setConfirmyn(String confirmyn) {
+		this.confirmyn = confirmyn;
+	}
+
+}
